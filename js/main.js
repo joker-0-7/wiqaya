a3d6ff0bd9dd6e579a3cb157f2a8cd48bd50faae
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2023-12-14 18:19:52 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const links = document.querySelectorAll("nav ul li a");
+const toUp = document.querySelector(".to-up");
 
 links.forEach((link) => {
   link.onclick = () => {
@@ -12,3 +13,15 @@ links.forEach((link) => {
 setInterval(() => {
   document.querySelector("#carouselExample2 .carousel-control-next").click();
 }, 5000);
+
+window.onscroll = () => {
+  if (window.scrollY > "200") {
+    toUp.classList.add("show");
+  } else {
+    toUp.classList.remove("show");
+  }
+};
+
+toUp.onclick = () => {
+  window.scrollTo(0, 0);
+};
